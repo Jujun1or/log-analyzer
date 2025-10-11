8f974121dbd402c85e29fc66b850fccf782137f6
s.a.khvatov <s.a.khvatov@tinkoff.ru>
2025-10-11 11:24:35 +0300

    feat: Initial Commit
    Moving files from existing project to the target repository

---
+++ src/test/java/academy/ApplicationTest.java
@@ -0,0 +1,15 @@
+package academy;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    @DisplayName("Базовая проверка работоспособности программы")
+    void happyPathTest() {
+        fail("Not implemented yet");
+    }
+}
